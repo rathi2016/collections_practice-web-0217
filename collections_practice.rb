780c45bb241d18d1d6d7d927def574f6e393d5d5
Rathi Nair-Das <rathind14@gmail.com>
2017-02-07 11:28:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort!{|x,y| y <=> x }
+end
+
+ def sort_array_char_count(array)
+  array.sort_by{|word| word.length}
+
+
+ end
+def swap_elements(arr)
+
+arr[1],arr[2] = arr[2],arr[1]
+  return arr
+end
+
+ def reverse_array(arr)
+   arr.reverse
+ end
+
+ def kesha_maker(array)
+   array.map! do |ele|
+     ele.sub(ele[2],'$')
+   end
+   array
+ end
+
+  def find_a(arr)
+    arr.select do |ele|
+      ele.downcase.start_with?('a')
+    end
+
+  end
+   def sum_array(arr)
+     sum = 0
+      arr.each {|ele| sum += ele}
+      return sum
+   end
+
+def add_s(array)
+  arr = []
+  array.each.with_index do |ele,idx|
+     if idx != 1
+      arr << ele +'s'
+     else
+      arr << ele
+     end
+
+    end
+ arr
+end
